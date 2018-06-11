5d4560344e7bb489d56663f8e78eabe758634eee
Irina <31949846+iraa305@users.noreply.github.com>
2018-06-11 16:43:51 +0300

    Update main.c

--- C/lab01/main.c
+++ C/lab01/main.c
@@ -1,26 +1,83 @@
-#include <stdio.h> //стандартный заголовочный файл ввода-вывода
-#include <math.h> //импортирование математической библиотеки
+//10.4
+#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+    int *a;
+    int i;
+    a = (int*)malloc(10 * sizeof(int));
+    for (i = 0; i <= 20; i++)
+        printf("%d %d\n", i, i*i);
+    free(a);
+    return 0;
+}
+
+//10.5
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, n;
+
+    printf("Введите количество товаров: ");
+    scanf("%i", &n);
+
+    struct storage{
+        char name[10];
+        double weight;
+        int price;
+    };
+    struct storage* data = (struct storage*)malloc(sizeof(struct storage) * n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Введите название товара:\n");
+        scanf("%s", &data[i].name);
+
+        printf("Введите вес товара:\n");
+        scanf("%lf", &data[i].weight);
+
+        printf("Введите цену товара:\n");
+        scanf("%d", &data[i].price);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("Название: %s Вес: %lf Цена: %d\n", data[i].name, data[i].weight, data[i].price);
+    }
+
+    free(data);
+
+    return 0;
+}
+
+//10.6
+#include <stdio.h>
+#include <string.h>
+
+char* allVowels(char* source, char* result) {
+    static const char CVowels[] = "EYUIOAeyuioa";
+    char* pstr = result;
+
+    for (; *source; ++source) {
+        if (strchr(CVowels, *source)) {
+            *pstr++ = *source;
+        }
+    }
+    *pstr = 0;
+
+    return result;
+}
+
 int main() {
-    double x, a, G, F, Y;
-    printf("Enter x:"); //Ввод числа x
-    scanf("%lf", &x); //Считывание введенной данной a
-    printf("Enter a:");//Ввод числа a
-    scanf("%lf", &a);//Считывание введенной данной x
-    G = -(8*(7*pow(a, 2) + 34*a*x - 5*pow(x, 2)))/(27*pow(x, 2) + 33*a*x + 10*pow(x, 2)); //вычисление формулы и присвоение G
-    printf("G=%f\n\n", G); //вывод результата присвоения
+    char source[256], result[256];
 
-    printf("Enter x:");
-    scanf("%lf", &x);
-    printf("Enter a:");
-    scanf("%lf", &a);
-    F = -(1)/(sin(72*pow(a, 2) - 5*a*x - 12*pow(x, 2) - M_1_PI/2));
-    printf("F=%f\n\n", F);
+    printf("text: ");
+    fgets(source, sizeof(source), stdin);
 
-    printf("Enter x:");
-    scanf("%lf", &x);
-    printf("Enter a:");
-    scanf("%lf", &a);
-    Y = log(42*pow(a, 2) + 53*a*x + 15*pow(x, 2)+1);
-    printf("Y=%f\n\n", Y);
+    printf("vowels: %s\n", allVowels(source, result));
+
     return 0;
 }

906077d992e83bcd9998807bbe2d1213ae3947c5
Irina <31949846+iraa305@users.noreply.github.com>
2018-06-11 16:52:38 +0300

    Update main.c

--- C/lab04/main.c
+++ C/lab04/main.c
@@ -1,112 +1,106 @@
+//trapezoidal_method
+#include<stdio.h>
+#include<math.h>
+
+/* Define the function to be integrated here: */
+double f(double x){
+    return x*x;
+}
+
+/*Program begins*/
+main(){
+    int n,i;
+    double a,b,h,x,sum=0,integral;
+    /*Ask the user for necessary input */
+    printf("\nEnter the no. of sub-intervals: ");
+    scanf("%d",&n);
+    printf("\nEnter the initial limit: ");
+    scanf("%lf",&a);
+    printf("\nEnter the final limit: ");
+    scanf("%lf",&b);
+    /*Begin Simpson's 1/3rd Procedure: */
+    h=fabs(b-a)/n;
+    for(i=1;i<n;i++){
+        x=a+i*h;
+        sum=sum+f(x);
+    }
+    integral=(h/2)*(f(a)+f(b)+2*sum);
+    /*Print the answer */
+    printf("\nThe integral is: %lf\n",integral);
+}
+
+//Euler_method
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+typedef double (*deriv_f)(double, double);
+#define FMT " %7.3f"
 
-    double a, x, z, l, G = 0, F = 0, Y = 0, max, min, d, v, min_value, max_value;
-    int c = 1, step = 0, b;
-    int j = 0;
-    double mas[100];
+void ivp_euler(deriv_f f, double y, int step, int end_t)
+{
+    int t = 0;
 
-    while ((c < 4) & (c > 0)) {
+    printf(" Step %2d: ", (int)step);
+    do {
+        if (t % 10 == 0) printf(FMT, y);
+        y += step * f(t, y);
+    } while ((t += step) <= end_t);
+    printf("\n");
+}
 
-        printf("1: Расчет G\n2: Расчет F\n3: Расчет Y\n4: Выход\n");
-        scanf("%d", &c);
+void analytic()
+{
+    double t;
+    printf("    Time: ");
+    for (t = 0; t <= 100; t += 10) printf(" %7g", t);
+    printf("\nAnalytic: ");
 
-        if (c == 4){
-            break;
-        }
+    for (t = 0; t <= 100; t += 10)
+        printf(FMT, 20 + 80 * exp(-0.07 * t));
+    printf("\n");
+}
 
-        printf("Введите границы изменения х.\nот:\n ");
-        scanf("%lf", &min);
-        printf("до:\n ");
-        scanf("%lf", &max);
+double cooling(double t, double temp)
+{
+    return -0.07 * (temp - 20);
+}
 
-        printf("Введите a:\n");
-        scanf("%lf", &a);
+int main()
+{
+    analytic();
+    ivp_euler(cooling, 100, 2, 100);
+    ivp_euler(cooling, 100, 5, 100);
+    ivp_euler(cooling, 100, 10, 100);
 
-        printf("Для ввода кол-ва шагов нажмите 1,\n для ввода самого шага введите 2\n");
-        scanf("%d", &b);
-
-        switch (b) {
-            case 1:
-                printf("Введите количество шагов:\n");
-                scanf("%d", &step);
-                v = (max - min) / (step - 1);
-                break;
-            case 2:
-                printf("Введите шаг\n");
-                scanf("%lf", &v);
-                break;
-            default:
-                printf("Вы ввели неправильное значение\n ");
-                return -1;
-        }
-        if (step > 100){
-            break;
-        }
-    x = min;
-    switch (c) {
-        case 1:
-            while (x <= max & j <= step) {
-                z = (27 * pow(x, 2) + 33 * a * x + 10 * pow(x, 2)); //Находим знаменатель
-                if (fabs(z) < 10e-6) { //Если равен нулю, выводим ошибку//
-                    printf("Не входит в область определения.\n");
-                } else {
-                    G = -(8 * (7 * pow(a, 2) + 34 * a * x - 5 * pow(x, 2))) / z; //Расчитываем ф-ию
-                    printf("x = %.3lf\t", x);
-                    printf("G = %.3lf\n", G);
-                }
-                x += v;
-                mas[j] = G;
-                j ++;
-            }
-            break;
-
-        case 2:
-            printf("Вычисление F\n");
-            while (x <= max & j <= step) {
-                d = (sin(72 * pow(a, 2) - 5 * a * x - 12 * pow(x, 2) - M_1_PI / 2));
-                if (fabs(d) < 10e-6) {
-                    printf("Не входит в область определения.\n");
-                } else {
-                    F = -(1) / d;
-                    printf("x = %lf\t", x);
-                    printf("F = %.3lf\n", F);
-                }
-                x += v;
-                mas[j] = F;
-                j ++;
-            }
-            break;
-
-        case 3:
-            while (x <= max & j <= step) {
-                l = (42 * pow(a, 2) + 53 * a * x + 15 * pow(x, 2) + 1);
-                if (fabs(l) < 10e-6) {
-                    printf("Не входит в область определения.\n");
-                } else {
-                    Y = log(42 * pow(a, 2) + 53 * a * x + 15 * pow(x, 2) + 1);
-                    printf("x = %.3lf\t", x);
-                    printf("Y = %.3lf\n", Y);
-                }
-                x += v;
-                    mas[j] = Y;
-                    j ++;
-                }
-                break;
-
-        default:
-                    printf("Неправильно выбрана функция.");
-
-            }
-        for (min_value =  mas[0], max_value = mas[0],j = 0;j < step;j++){
-            if (mas[j] > max_value)
-                max_value = mas[j];
-            if (mas[j] < min_value)
-                min_value = mas[j];
-        }
-        printf(" Максимальное значение = %9.3lf\n Минимальное значение = %9.3lf\n", max_value, min_value);
-    }
     return 0;
 }
+
+//reimann sum
+#include <stdio.h>
+
+
+double f(double x){
+    return x * x;
+}
+
+int main() {
+    double lower_limit;
+    printf("Enter lower limit: \n");
+    scanf("%lf", &lower_limit);
+    double upper_limit;
+    printf("Enter upper limit: \n");
+    scanf("%lf", &upper_limit);
+    double accuracy;
+    printf("Enter accuracy: \n");
+    scanf("%lf", &accuracy);
+    double i = lower_limit;
+    double area = 0;
+
+    while (i < upper_limit){
+        area += f(i) * accuracy;
+        i += accuracy;
+    }
+    printf("Calculated area:%.3f\n", area);
+    return 0;
+}
+
